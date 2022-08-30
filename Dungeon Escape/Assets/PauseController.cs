ddffc8f9f6bfd5f4eb0794e1557e577a5ddddad2
unknown <rizner.josip@gmail.com>
2022-08-30 16:30:35 +0200

    Adding option to pause the game

---
+++ Dungeon Escape/Assets/PauseController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseController : MonoBehaviour
+{
+    public static bool gameIsPaused;
+
+    [SerializeField] GameObject pauseMenuUI;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(gameIsPaused){
+                Resume();
+            }
+            else{
+                PauseGame();
+            }
+        }
+    }
+    void PauseGame ()
+    {
+        Time.timeScale = 0f;
+        pauseMenuUI.SetActive(true);
+        gameIsPaused = true;
+    }
+
+    public void Resume(){
+        Time.timeScale = 1;
+        pauseMenuUI.SetActive(false);
+        gameIsPaused = false;
+    }
+}
